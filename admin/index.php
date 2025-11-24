7b01e852a81249d21b1b45fd24c02f0e8f9540b6
Putra Danialzulqarnain <putradanialzul765@gmail.com>
2025-11-24 15:20:00 +0800

    Add admin page

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+    if(isset($_POST["secret"]))
+        $admin_secret = $_POST["secret"];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Page - PIKAA</title>
+        <link rel="stylesheet" src="style.css">
+    </head>
+    <body>
+        <img id="logo" src="/media/pikaa.png" alt="pikaa logo">
+    </body>
+</html>
